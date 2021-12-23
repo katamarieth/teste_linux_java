bb4401fad2585abbcd9a8066ddaea12bfbea8d74
Katherine Mariete <katamarieth@gmail.com>
2021-12-22 23:40:44 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato("miau","preto","3");
+        System.out.println(gato);
+       /* int a = 5;
+        int b = 3;
+        System.out.println("Hello world Linux!!" + ((a+b)));*/
+    }
+}
